0b3c10e4218c895da652ea96106cfe019da9786c
pyuser <enes.karakas.e@gmail.com>
2021-08-18 14:12:33 +0300

    code review, extensebility

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const BRUSH_TIME = 1500;
 const activeToolEl = document.getElementById("active-tool");
 const brushColorBtn = document.getElementById("brush-color");
 const brushIcon = document.getElementById("brush");
@@ -70,6 +71,10 @@ function switchToBrush() {
   displayBrushSize();
 }
 
+function switchToBrushsetTimeout(time) {
+  setTimeout(switchToBrush, time);
+}
+
 // Create Canvas
 function createCanvas() {
   canvas.width = window.innerWidth;
@@ -86,7 +91,7 @@ clearCanvasBtn.addEventListener("click", () => {
   drawnArray = [];
   // Active Tool
   activeToolEl.textContent = "Canvas Cleared";
-  setTimeout(switchToBrush, 1500);
+  switchToBrushsetTimeout(BRUSH_TIME);
 });
 
 // Draw what is stored in DrawnArray
@@ -171,7 +176,7 @@ saveStorageBtn.addEventListener("click", () => {
 
   // Active Tool
   activeToolEl.textContent = "Canvas Saved";
-  setTimeout(switchToBrush, 1500);
+  switchToBrushsetTimeout(BRUSH_TIME);
 });
 
 // Load from Local Storage
@@ -183,10 +188,10 @@ loadStorageBtn.addEventListener("click", () => {
 
     // Active Tool
     activeToolEl.textContent = "Canvas Loaded";
-    setTimeout(switchToBrush, 1500);
+    switchToBrushsetTimeout(BRUSH_TIME);
   } else {
     activeToolEl.textContent = "No Canvas Found";
-    setTimeout(switchToBrush, 1500);
+    switchToBrushsetTimeout(BRUSH_TIME);
   }
 });
 
@@ -196,7 +201,7 @@ clearStorageBtn.addEventListener("click", () => {
     localStorage.removeItem("savedCanvas");
     // Active Tool
     activeToolEl.textContent = "Local Storage Cleared";
-    setTimeout(switchToBrush, 1500);
+    switchToBrushsetTimeout(BRUSH_TIME);
   }
 });
 
@@ -206,7 +211,7 @@ downloadBtn.addEventListener("click", () => {
   downloadBtn.download = "download-example.jpeg";
   // Active Tool
   activeToolEl.textContent = "Image File Saved";
-  setTimeout(switchToBrush, 1500);
+  switchToBrushsetTimeout(BRUSH_TIME);
 });
 
 // // Event Listener
